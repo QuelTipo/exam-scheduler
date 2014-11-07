238924cd28ea4b45b84201113db5a52dea8a07a0
jdspoone <jeff@Jeffreys-MacBook-Pro.local>
2014-11-06 17:39:19 -0700

    Added a_enrolled method
    Looking through his parser code I believe that this is the right way to
    handle this

--- CPSC433/src/examSchedule/Environment.java
+++ CPSC433/src/examSchedule/Environment.java
@@ -495,7 +495,15 @@ public class Environment extends PredicateReader implements ExamSchedulePredicat
 	}
 
 	@Override
-	public void a_enrolled(String student, Vector<Pair<ParamType, Object>> list) {
+	public void a_enrolled(String s, Vector<Pair<ParamType, Object>> list) {
+		// The object part of the pair is the vector courses and lectures
+		// This means the vector should only have 1 pair in it
+		assert(list.size() == 1);
+		
+		Vector<String> vec = (Vector<String>)list.get(0).getValue();
+		for (int i = 0; i < vec.size(); i+=2) {
+			a_enrolled(s, vec.get(i), vec.get(i+1));
+		}
 		
 	}
 	
