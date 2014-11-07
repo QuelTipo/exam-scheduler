4b11f249a7b1ef89fbb117d467423f7af7ed045e
jdspoone <jeff@Jeffreys-MacBook-Pro.local>
2014-11-06 19:51:11 -0700

    Double oops

--- CPSC433/src/examSchedule/Course.java
+++ CPSC433/src/examSchedule/Course.java
@@ -1,30 +1,13 @@
 package examSchedule;
 
-import java.util.TreeSet;
 import examSchedule.parser.Entity;
 
 public class Course extends Entity {
 
 	public Course(String name) {
 		super(name);
-		
-		lectures = new TreeSet<String>();
 	}
-		
-	public boolean checkForLecture(String l) {
-		for (String lec : lectures) {
-			if (l == lec) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	public void update(String l) {
-		if (checkForLecture(l))
-			lectures.add(l);
-	}
-	
+				
 	public String toString() {
 		return "course(" + getName() + ")";
 	}
