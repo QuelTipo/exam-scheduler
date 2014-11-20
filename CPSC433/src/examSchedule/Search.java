7eb4ba521fc2d9b2c6aedef0ea18e00d47a59f27
Jeff Spooner <jdspoon@telus.net>
2014-11-19 18:18:53 -0700

    Added Search class
    Added skeleton Search class to project

---
+++ CPSC433/src/examSchedule/Search.java
@@ -0,0 +1,13 @@
+package examSchedule;
+
+public class Search {
+
+	private Environment environment;
+	
+	// Default constructor
+	public Search(Environment env) {
+		
+		this.environment = env;
+	}
+	
+}
