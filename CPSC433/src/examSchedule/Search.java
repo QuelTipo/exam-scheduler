e263509f9a25488c2b5d0a51223e3b42c8114542
Jeff Spooner <jdspoon@telus.net>
2014-11-23 20:46:40 -0700

    Expanded Search class
    Began fleshing out Search class
    Added a setup method to be called before beginning the search proper

--- CPSC433/src/examSchedule/Search.java
+++ CPSC433/src/examSchedule/Search.java
@@ -1,13 +1,37 @@
 package examSchedule;
 
+import java.util.TreeSet;
+
 public class Search {
 
 	private Environment environment;
+	private SolutionGenerator generator;
+	private TreeSet<Solution> solutions;
+	private Solution bestSolution;
 	
 	// Default constructor
 	public Search(Environment env) {
 		
 		this.environment = env;
+		this.generator = new SolutionGenerator(environment);
 	}
+	
+	// This method will do the necessary setup for our set based search
+	public void setup() {
+		
+		// Begin by populating our solution set with random, valid solutions
+		for (int i = 0; i < 5; ++i) {
+			Solution solution = generator.buildSolution();
+			solutions.add(solution);
+		}
+		
+		// Now set our initial best solution
+		for (Solution solution : solutions) {
+			if (bestSolution == null || solution.getPenalty() < bestSolution.getPenalty())
+				bestSolution = solution;
+		}
+	}
+	
+
 	
 }
